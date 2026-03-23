eb5225305c184dd7ec0a005e2ca71784e9311b5c
Shubham Vishwakarma <sv8112004@gmail.com>
2026-03-23 09:55:31 +0530

    Custom Hook :01

---
+++ customhooks/src/App.jsx
@@ -0,0 +1,13 @@
+import CustomHookOne from "./CustomHookOne"
+function App() {
+
+  return (
+    <>
+      <h1>Custom Hooks : 23/03/2026</h1>
+      <CustomHookOne /> {/*First Custom Hook -> Here we created counter custom Hook */}
+
+    </>
+  )
+}
+
+export default App;
---
+++ customhooks/src/CustomHookOne.jsx
@@ -0,0 +1,44 @@
+import {useState} from 'react';
+
+function useCounter(){
+    const[count, setCount] =useState(0);
+
+    function increaseCount(){
+        setCount(count=>count+1);
+    }
+    function decreaseCount(){
+        setCount(count=>count-1);
+    }
+    
+    return{
+        count:count, 
+        increaseCount:increaseCount,
+        decreaseCount:decreaseCount
+    }
+}
+
+function Counter(){
+     const{count, increaseCount, decreaseCount}=useCounter();
+    return(
+        <div>
+        <button onClick={increaseCount} style ={{backgroundColor:"#2fe635", borderRadius:"20px", margin:"20px",cursor:"pointer"}}>Increase </button>
+        <button onClick={decreaseCount} style ={{backgroundColor:"#ff3a3a", borderRadius:"20px", margin:"20px", cursor:"pointer"}}>Decrease </button>
+        <span style={{backgroundColor:"#e7d9f5", borderRadius:"5px", padding:"10px", margin:"20px"}}>Value: {count}
+        </span>
+        </div>
+    )
+    
+}
+function customHookOne(){
+    return(
+        <>
+        <h2 style ={{color:"#0d2c88"}}>1. Custom Counter Hook for Clean Code [CustomHookOne.jsx]</h2>
+        <Counter />
+        <Counter />
+        <Counter />
+        </>
+    )
+   
+}
+
+export default customHookOne ;
